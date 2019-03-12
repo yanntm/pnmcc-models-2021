0a20069a58d637616f508434091c706ada885f65
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2019-03-12 15:13:47 +0100

    less verbose

--- csv_to_control.pl
+++ csv_to_control.pl
@@ -4,13 +4,13 @@
 my @index = ("00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15");
 
 while (my $line = <STDIN>) {
-     print $line;
+    # print $line;
     chomp $line;
   my @fields = split /,/, $line;
   my $prefix = @fields[0]."-".@fields[1];
   my @verdicts = split //, @fields[2];
 
-  print "Verdicts ($#verdicts) = @verdicts \n";
+   # print "Verdicts ($#verdicts) = @verdicts \n";
   if ($#verdicts != 15 && $#verdicts != 0) {
     @verdicts = split / /, @fields[2];
     if ($#verdicts != 15) {
