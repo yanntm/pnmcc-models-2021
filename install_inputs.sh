8e1797f8f7618e257f74118e37ad93800acaf786
ythierry <ythierry@localhost>
2019-05-18 14:56:56 +0200

    going for 2019 versions

--- install_inputs.sh
+++ install_inputs.sh
@@ -9,16 +9,16 @@ cd website
 # grab the vmdk file image for all inputs
 mkdir INPUTS
 cd INPUTS
-wget --progress=dot:mega http://mcc.lip6.fr/2018/archives/mcc2018-input.vmdk.tar.bz2
-tar xvjf mcc2018-input.vmdk.tar.bz2
-../../7z e mcc2018-input.vmdk
-rm mcc2018-input.vmdk
-rm mcc2018-input.vmdk.tar.bz2
+wget --progress=dot:mega http://mcc.lip6.fr/archives/mcc2019-input.vmdk.tar.bz2
+tar xvjf mcc2019-input.vmdk.tar.bz2
+../../7z e mcc2019-input.vmdk
+rm mcc2019-input.vmdk
+rm mcc2019-input.vmdk.tar.bz2
 cd ..
 
 # create oracle files
 mkdir oracle
-wget --progress=dot:mega https://mcc.lip6.fr/2018/archives/raw-result-analysis.csv.zip
+wget --progress=dot:mega https://mcc.lip6.fr/archives/raw-result-analysis.csv.zip
 unzip raw-result-analysis.csv.zip
 cat raw-result-analysis.csv | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | ../csv_to_control.pl
 cat raw-result-analysis.csv | grep ReachabilityDeadlock | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | sed 's/ReachabilityDeadlock/GlobalProperties/g' | ../csv_to_control.pl
