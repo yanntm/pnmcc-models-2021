a7d48198555a2bf6a97e6824a8390e9dfed08c17
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-09-25 18:44:09 +0200

    retire GlobalProperties case : alias for Deadlock some years

--- install_inputs.sh
+++ install_inputs.sh
@@ -18,7 +18,6 @@ mkdir oracle
 wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/archives/raw-result-analysis.csv.zip
 unzip raw-result-analysis.csv.zip
 cat raw-result-analysis.csv | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | ../csv_to_control.pl
-cat raw-result-analysis.csv | grep ReachabilityDeadlock | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | sed 's/ReachabilityDeadlock/GlobalProperties/g' | ../csv_to_control.pl
 mv *.out oracle/
 #rm -f raw-result-analysis.csv*
 
