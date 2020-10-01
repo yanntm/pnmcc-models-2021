0406d7f6b12c93c78361acb61b16579a09c38485
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2020-10-01 16:18:25 +0200

    patches for 2020 bad consensus
    Sudoku-COL, meh, my bad, finiteIntRange was incorrectly parsed in ITS-xxx with exclusive lower bound. Leads to a bunch of bad consensus results, just remove the whole mess.
    
    Angio issue in LTLF, inspected manually.

--- install_inputs.sh
+++ install_inputs.sh
@@ -19,6 +19,16 @@ wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/archives/raw
 unzip raw-result-analysis.csv.zip
 cat raw-result-analysis.csv | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | ../csv_to_control.pl
 mv *.out oracle/
+
+# Due to parse errors of ITS-Tools+ITS-Lola that were not always interpreted as such in 2020
+# consensus, and thus oracles on this model are unreliable.
+rm oracle/Sudoku-COL*
+
+# after manual examination, this consensus verdict (with weak support) is also wrong
+# formula is reduced to true by e.g. Spot : https://spot.lrde.epita.fr/app/
+# try it : X !X !(X G "k38" | F !X "k38")
+sed -i -e "s/Angiogenesis-PT-15-14 FALSE/Angiogenesis-PT-15-14 TRUE/" oracle/Angiogenesis-PT-15-LTLF.out
+
 #rm -f raw-result-analysis.csv*
 
 cd oracle
