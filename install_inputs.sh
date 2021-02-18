62879d62834bace8e6a76caeae741c48d3785ab4
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 15:10:06 +0100

    files have moved to 2020 folder since 2021 site is up

--- install_inputs.sh
+++ install_inputs.sh
@@ -7,7 +7,7 @@ cd website
 
 # grab the vmdk file image for all inputs
 mkdir INPUTS
-wget --no-check-certificate --progress=dot:mega http://mcc.lip6.fr/archives/mcc2020-input.vmdk.tar.bz2
+wget --no-check-certificate --progress=dot:mega http://mcc.lip6.fr/2020/archives/mcc2020-input.vmdk.tar.bz2
 tar xvjf mcc2020-input.vmdk.tar.bz2
 ../7z e mcc2020-input.vmdk
 ../ext2rd 0.img ./:INPUTS
@@ -15,7 +15,7 @@ rm -f *.vmdk 0.img *.bz2 1
 
 # create oracle files
 mkdir oracle
-wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/archives/raw-result-analysis.csv.zip
+wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/2020/archives/raw-result-analysis.csv.zip
 unzip raw-result-analysis.csv.zip
 cat raw-result-analysis.csv | cut -d ',' -f2,3,16 | grep -v "?" | sort | uniq | ../csv_to_control.pl
 mv *.out oracle/
